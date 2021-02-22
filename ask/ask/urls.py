e820fb595e250c76bbc55b848d6696d3de4a4922
SemenAgarkov <77958429+SemenAgarkov@users.noreply.github.com>
2021-02-22 09:59:56 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from qa.views import test 
 
 urlpatterns = [
-    #url(r'^$', test, name='test'),
+    url(r'^$', test, name='test'),
     url(r'^login/$', test, name='test'),
     url(r'^signup/$', test, name='test'),
     url(r'^question/\d+/', test, name='test'),
