49a3d39b92a4500031d5e0f87595e6af4c7a3728
semenagarkov <semen.agarkov@gmail.com>
2021-02-28 09:53:41 +0000

    Step 2.6

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -3,19 +3,19 @@ from django import forms
 class AskForm(forms.Form):
     title = forms.CharField(max_length=100)
     text = forms.CharField(widget=forms.Textarea)
-
-    def __init__(self, user, *args, **kwargs):
+'''
+    def __init__(self, user, **kwargs):
         self._user = user
-        super(AddPostForm, self).__init__(*args, **kwargs)
+        super(AddPostForm, self).__init__(**kwargs)
 
     def clean(self):
         if self._user.is_banned:
-            raise ValidationError(u'Доступ ограничен')
+            raise ValidationError(u'Access denied')
 
     def save(self):
         self.cleaned_data['author'] = self._user
         return Post.objects.create(**self.cleaned_data)
-
+'''
 
     def clean_text(self):
         pass
@@ -28,21 +28,21 @@ class AskForm(forms.Form):
 class AnswerForm(forms.Form):
     text = forms.Textarea(default="")
     question = forms.CharField(widget=forms.Textarea)
-
-    def __init__(self, user, *args, **kwargs):
+'''
+    def __init__(self, user, **kwargs):
         self._user = user
-        super(AddPostForm, self).__init__(*args, **kwargs)
+        super(AddPostForm, self).__init__(**kwargs)
 
 
     def clean(self):
         if self._user.is_banned:
-            raise ValidationError(u'Доступ ограничен')
+            raise ValidationError(u'Access denied')
 
 
     def save(self):
         self.cleaned_data['author'] = self._user
         return Post.objects.create(**self.cleaned_data)
-
+'''
     def clean_question(self):
         question_id = self.cleaned_data['question']
         try:
